63cb8cc8a2575d4c936dd83288b3b9f9c75b00fa
PatricioJoandet <patriciojoandet@gmail.com>
2022-07-27 21:04:15 -0300

    itemDetail started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar'
 import ItemListContainer from './components/ItemListContainer'
+import Item from './components/Item'
 
 function App() {
   return (
     <div className="App">  
       <NavBar />
-      <ItemListContainer section="Destacados"/>
+      <ItemListContainer section="Destacados" /> 
+      
     </div>
   );
 }
---
+++ src/components/ItemDetailContainer.js
@@ -0,0 +1,14 @@
+import itemDetailMock from '../utl/itemDetailMock'
+import { useState, useEffect } from 'react'
+
+const itemDetailContainer = () =>{
+
+    const [item, setItem] = useState([])
+    
+    const getItem = new Promise(resolve,reject)
+    return(
+        <div>
+            
+        </div>
+    )
+}
