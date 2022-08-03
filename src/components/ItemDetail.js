9eebac2243eff594880f49e66da96de0cc0f6f72
PatricioJoandet <patriciojoandet@gmail.com>
2022-08-03 15:51:58 -0300

    item detail with secondary imgs

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -7,15 +7,25 @@ const ItemDetail = ({data}) =>{
     const {title, year, community, genres} = data
     data.price = Math.trunc((community.have - community.want)*.8+2000)
     let stock = data.num_for_sale
-
-    console.log(data.tracklist)
-
+    let i=0
+    let imgs = []
+    for(i;i<4;i++){
+        imgs[i]= data.images[i].resource_url  
+    }
+    
     return(
         <div className="itemDetailContainer">
             <div>
-                <div>
+                <div className="mainImg">
                     <img className="itemDetailImg" src={data.images[0].resource_url} alt={`Portada de ${title}`}/>
                 </div>
+                <div className="altImg">               
+                    <img src={imgs[0]}/>
+                    <img src={imgs[1]}/>
+                    <img src={imgs[2]}/>
+                    <img src={imgs[3]}/>
+                </div>
+
             </div>
             <div className="itemDetailInfo">
                 <div>
