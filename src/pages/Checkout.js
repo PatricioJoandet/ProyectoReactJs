d9b9ba07f6d0c7294da204979a4d035a81d4306c
Pato Joandet <patriciojoandet@gmail.com>
2022-09-09 12:10:22 -0300

    merge

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -45,100 +45,105 @@ const Checkout = () =>{
 		setSuccess(orderDoc.id)
 	}
 
-    if (cart.length===0){ 
-        return(
-            <div className='emptyCheckout'>
-                <h2>carrito vacio</h2>
-                <Link to={"/"}><button>Volver</button></Link>
-            </div>
-            )}else{
-        return(
-            <div>
-
-                <div className='checkoutTitle'>
-                    <h1>Checkout</h1>
-                                    {cart.length>0 ? <button onClick={clear}>eliminar carrito</button> : <h2>carrito vacio</h2>}		
-                </div>
-                <div>
-                    <div className="checkoutWrapper">
-                                        <div className='checkoutHeader'>
-                                            <div className='checkoutProduct'>
-                                                <h3>Producto</h3>
-                                            </div>
-                                            <div className='checkoutQty'>
-                                                <h3>Cantidad</h3>
-                                            </div>
-                                            <div className='checkoutPrice'>
-                                                <h3>Precio</h3>
-                                            </div>
-                                            <div className='checkoutTotal'>
-                                                <h3>Total</h3>
-                                            </div>
-                                        </div>
-                        {cart.map(product=>
-                            <div key={product.id} className='checkoutItem'>
-                                                            <div className='checkoutItemInfo'>
-                                                                <div className='checkoutProductImg'>
-                                                                    <img src={product.images[0].resource_url} alt={`Imagen de ${product.title}`}/>
-                                                                </div>
-                                                                <div className='checkoutProductTitle'>
-                                                                    <h2>{product.title}</h2>
-                                                                    <h3>{product.artists_sort}</h3>
-                                                                </div>
-                                                                <div className='checkoutProductQty'>
-                                                                    <span>{product.QtyCounter}</span>
-                                                                    <button onClick={()=>removeFromCart(product.id)}>Borrar</button>
-                                                                </div>
-                                                                <div className='checkoutProductPrice'>
-                                                                    <span>${product.price}</span>
-                                                                </div>
-                                                                <div className='checkoutProductTotal'>
-                                                                    <span>${product.price * product.QtyCounter}</span>
-                                                                </div>
-                                                            </div>
-                            </div>)}
-                        <div className='checkoutTotalTotal'>Total: {`$ ${total}`}</div>
-                        <button onClick={() => setShowModal(true)}>Comprar</button>
-                    </div>
-                </div>
-                {showModal &&
-									<Modal title="DATOS DE COMPRA" close={()=>setShowModal()}>
-										{success ?(
-											<>
-										<h2>Orden OK</h2>
-										<p>ID DE COMPRA: {success}</p>
-										<button onClick={clear}>FINALIZAR</button>
-										</>
-										):
-										
-										<form onSubmit={submitData}>
-											<input
-												type='text'
-												name='name'
-												placeholder='Nombre'
-												onChange={handleChange}
-												value={formData.name}
-												/>
-											<input
-												 type='number'
-												 name='phone'
-												 placeholder='Telefono'
-												 value={formData.phone}
-												 onChange={handleChange}
-												/>
-											<input 
-												type='email'
-												name='email'
-												placeholder='email'
-												value={formData.email}
-												onChange={handleChange}
-												/>
-												<button type="submit"> ENVIAR </button>
-										</form>
-										}
-									</Modal>}
-            </div>
-        )}
+	if (cart.length===0){ 
+  	return(
+    	<div className='emptyCheckout'>
+      	<h2>carrito vacio</h2>
+        <Link to={"/"}><button>Volver</button></Link>
+      </div>
+ 	 )
+	}else{
+    return(
+    	<div className=''>
+      	<div className='checkoutHead'>
+					<div className='checkoutMyCart'>
+						<h1>Mi Carrito</h1>
+						<button className='clearCartBtn' onClick={clear}>eliminar carrito</button>
+					</div>
+					<div className='checkoutBuy'>
+						<button> Iniciar Compra</button>
+					</div>
+    		</div>
+      	<div>
+					<div className="checkoutWrapper">
+						<div className='checkoutHeader'>
+							<div className='checkoutProduct'>
+								<h3>Producto</h3>
+							</div>
+							<div className='checkoutQty'>
+								<h3>Cantidad</h3>
+							</div>
+							<div className='checkoutPrice'>
+								<h3>Precio</h3>
+							</div>
+							<div className='checkoutTotal'>
+								<h3>Total</h3>
+							</div>
+						</div>
+						{cart.map(product=>
+							<div key={product.id} className='checkoutItem'>
+								<div className='checkoutItemInfo'>
+									<div className='checkoutProductImg'>
+										<img src={product.images[0].resource_url} alt={`Imagen de ${product.title}`}/>
+									</div>
+									<div className='checkoutProductTitle'>
+										<h2>{product.title}</h2>
+										<h3>{product.artists_sort}</h3>
+									</div>
+									<div className='checkoutProductQty'>
+										<span>{product.QtyCounter}</span>
+										<button onClick={()=>removeFromCart(product.id)}>Borrar</button>
+									</div>
+									<div className='checkoutProductPrice'>
+										<span>${product.price}</span>
+									</div>
+									<div className='checkoutProductTotal'>
+										<span>${product.price * product.QtyCounter}</span>
+									</div>
+								</div>
+							</div>
+						)}
+						<div className='checkoutTotalTotal'>Total: {`$ ${total}`}</div>
+							<button onClick={() => setShowModal(true)}>Comprar</button>
+						</div>
+					</div>
+					{showModal &&
+						<Modal title="DATOS DE COMPRA" close={()=>setShowModal()}>
+							{success ?(
+								<>
+									<h2>Orden OK</h2>
+									<p>ID DE COMPRA: {success}</p>
+									<button onClick={clear}>FINALIZAR</button>
+								</>
+							):				
+								<form onSubmit={submitData}>
+									<input
+										type='text'
+										name='name'
+										placeholder='Nombre'
+										onChange={handleChange}
+										value={formData.name}
+									/>
+									<input
+										type='number'
+										name='phone'
+										placeholder='Telefono'
+										value={formData.phone}
+										onChange={handleChange}
+									/>
+									<input 
+										type='email'
+										name='email'
+										placeholder='email'
+										value={formData.email}
+										onChange={handleChange}
+									/>
+									<button type="submit"> ENVIAR </button>
+								</form>
+							}
+						</Modal>}
+      </div>
+    )}
 }
 
 export default Checkout
